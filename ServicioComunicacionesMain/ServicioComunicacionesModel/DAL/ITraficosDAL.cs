96a9b7cad0b29c06fce68daaedb62abe6017e26c
maja90k <bdh-99@hotmail.cl>
2021-05-12 00:23:48 -0400

    primer commit, creacion de proyecto

---
+++ ServicioComunicacionesMain/ServicioComunicacionesModel/DAL/ITraficosDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioComunicacionesModel.DAL
+{
+    public interface ITraficosDAL
+    {
+        void Save();//falta el nombre del objeto
+        List<> GetAll();//falta la lista  con el nombre del objeto
+
+    }
+}
