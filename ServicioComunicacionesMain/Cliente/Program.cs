2528e77cea78c6272cbdf9c3e6f9c6759dfdc419
Patricio Reyes <reyes.patricio.p@gmail.com>
2021-05-20 23:44:21 -0400

    actualizacion cliente

--- ServicioComunicacionesMain/Cliente/Program.cs
+++ ServicioComunicacionesMain/Cliente/Program.cs
@@ -1,8 +1,10 @@
-﻿using ServicioComunicacionesModel.DAL.Lecturas;
+﻿using Cliente.Sockets;
+using ServicioComunicacionesModel.DAL.Lecturas;
 using ServicioComunicacionesModel.DTO;
 using SocketsUtils;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,109 +13,64 @@ namespace Cliente {
 
    partial class Program
    {
-   
-
-    public void ClienteConsumo()
+     
+    static void Main(string[] args)
     {
-
-
-        
-        static ILecturasDAL dal = LecturasDALFactory.CreateDal();
-        private static ServerSocket server;
-
-        MedidorConsumoSocket = new ClienteSocket(ipServidor, puerto);
-
-        if (MedidorConsumoSocket.Conectar())
-        {
-            Console.WriteLine("Conectado exitosamente");
-
-            string lectura = MedidorConsumoSocket.Leer();
-            Console.WriteLine("S:{0}", mensaje);
-
-            Lectura = MedidorConsumoSocke.Leer();
-
-            String nromedidor;
-            do
-            {
-                Console.WriteLine("S:{0}", lectura);
-                nromedidor = Console.ReadLine().Trim();
-
-                ValidarNroMedidor(nromedidor);
-
-                if (!ValidarNroMedidor(nromedidor))
-                {
-                    Console.WriteLine(nromedidor + "|" + "ERROR");
-                }
-
-
-            } while (!ValidarNroSerieCont(nromedidor));
-        }
-
-        public void ClienteTrafico()
-        {
-
-        }
-
-        static void Main(string[] args)
-        {
-
             string ipServidor = ConfigurationManager.AppSettings["servidor"];
             int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
 
             Console.WriteLine("Conectando a {0} en puerto {1}", ipServidor, puerto);
-            Console.WriteLine("Indique que tipo de cliente es");
-            Console.WriteLine("1. Consumo");
-            Console.WriteLine("2. Trafico");
-            string opcion = Console.ReadLine().Trim();
-            switch (opcion)
-            {
-                case "1":
-                    ClienteConsumo()
-                    break;
-                case "2":
-                    ClienteTrafico()
-                    break;
-                default:
-                    ;
-                    break;
-            }
-            return continuar;
-
-
-
 
             ClienteSocket clienteSocket = new ClienteSocket(ipServidor, puerto);
+
             if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Error al establecer comunicacion");
+            }
+            else
             {
                 Console.WriteLine("Conectado exitosamente");
 
-                string lectura = clienteSocket.Leer();
-                Console.WriteLine("S:{0}", mensaje);
+                string datos = clienteSocket.Leer();
+                Console.WriteLine("S:{0}", datos);
 
-                Lectura = clienteSocket.Leer();
+                datos = clienteSocket.Leer();
 
                 String nromedidor;
                 do
                 {
-                    Console.WriteLine("S:{0}", mensaje);
-                    nroSerCont = Console.ReadLine().Trim();
+                    Console.WriteLine("S:{0}", datos);
+                    nromedidor = Console.ReadLine().Trim();
 
-                    ValidarNroSerieCont(nroSerCont);
+                    ValidarNroMedidor(nromedidor);
 
-                    if (!ValidarNroMedidor(nroSerCont))
+                    if (!ValidarNroMedidor(nromedidor))
                     {
-                        Console.WriteLine(nroSerCont + "|" + "ERROR");
+                        Console.WriteLine(nromedidor + "|" + "ERROR");
                     }
 
 
-                } while (!ValidarNroSerieCont(nroSerCont));
+                } while (!ValidarNroMedidor(nromedidor));
 
-                clienteSocket.Escribir(nroSerCont);
-                Console.WriteLine("-----------------------------------------------------------");
-                Console.WriteLine("-----------------------------------------------------------");
+                clienteSocket.Escribir(nromedidor);
 
-                mensaje = clienteSocket.Leer();
-                Console.WriteLine("S:{0}", mensaje);
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("Seleccion el tipo de cliente que es: ");
+                Console.WriteLine("1. Consumo");
+                Console.WriteLine("2. Trafico");
+
+                string tipo;
+                do
+                {
+                    tipo = Console.ReadLine().Trim();
+
+                    if (!ValidarTipo(tipo))
+                    {
+
+                    }
+                } while (!ValidarTipo(tipo));
+               
+
 
                 string nroSerie = "";
                 do
@@ -132,10 +89,6 @@ namespace Cliente {
 
                     }
 
-                    if (nroSerie != nroSerCont)
-                    {
-                        Console.WriteLine("no corresponde al ID logeado");
-                    }
 
                 } while (!ValidarNroSerie(nroSerie) || nroSerie != nroSerCont);
 
@@ -143,8 +96,8 @@ namespace Cliente {
                 clienteSocket.Escribir(nroSerie);
 
 
-                mensaje = clienteSocket.Leer();
-                Console.WriteLine("el servidor respondio: {0}", mensaje);
+                datos = clienteSocket.Leer();
+                Console.WriteLine("el servidor respondio: {0}", datos);
 
                 String hr = DateTime.Now.ToString("ss/mm/HH/dd/MM/yyyy");
                 string fechaEstado = "";
@@ -162,12 +115,28 @@ namespace Cliente {
 
                 } while (!ValidarFecha(fechaEstado));
 
-                string[] datos = hr.Split('/');
-                String hrfecha = fechaEstado + "-" + datos[2] + "-" + datos[1] + "-" + datos[0];
+                string[] dat = hr.Split('|');
+                String hrfecha = fechaEstado + "-" + dat[2] + "-" + dat[1] + "-" + dat[0];
                 clienteSocket.Escribir(hrfecha);
 
-                mensaje = clienteSocket.Leer();
-                Console.WriteLine("el servidor respondio: {0}", mensaje);
+                datos = clienteSocket.Leer();
+                Console.WriteLine("el servidor respondio: {0}", datos);
+                
+
+                do
+                {
+                    valor = Console.ReadLine().Trim();
+                    Validarvalor(valor);
+
+                    if (!Validarvalor(valor))
+                    {
+                        Console.WriteLine("Ingrese un numero de 1 a 1000");
+
+                    }
+
+
+                } while (!Validarvalor(valor));
+
 
                 string estado = "";
                 do
@@ -179,45 +148,19 @@ namespace Cliente {
 
                     if (!ValidarEstado(estado))
                     {
-                        Console.WriteLine("Ingrese solamente 1,0,-1");
+                        Console.WriteLine("Ingrese solamente -1,0,1,2");
                     }
 
                 } while (!ValidarEstado(estado));
-
-
                 clienteSocket.Escribir(estado);
 
-                mensaje = clienteSocket.Leer();
-                Console.WriteLine("el servidor respondio: {0}", mensaje);
-                String nivell = "";
+                datos = clienteSocket.Leer();
+                Console.WriteLine("el servidor respondio: {0}", datos);
 
-                do
-                {
-                    nivell = Console.ReadLine().Trim();
-                    ValidarNivLlendo(nivell);
-
-                    if (!ValidarNivLlendo(nivell))
-                    {
-                        Console.WriteLine("Ingrese un numero de 1 a 100");
-
-                    }
-
-
-                } while (!ValidarNivLlendo(nivell));
-
-                clienteSocket.Escribir(nivell + "%");
-
-                mensaje = clienteSocket.Leer();
-                Console.WriteLine("el servidor respondio: {0}", mensaje);
-
-                clienteSocket.Escribir("MENSAJE REGISTRADO");
+                clienteSocket.Escribir("Lectura Registrada");
 
                 clienteSocket.Desconectar();
                 Console.ReadKey();
 
             }
-            else
-            {
-                Console.WriteLine("Error al establecer comunicacion");
-            }
-}
+        }
---
+++ ServicioComunicacionesMain/Cliente/Sockets/ClienteSocket.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente.Sockets
+    {
+        public class ClienteSocket
+        {
+            private int puerto;
+            private string servidor;
+            private Socket conCliente;
+            private StreamReader reader;
+            private StreamWriter writer;
+
+            public ClienteSocket(string servidor, int puerto)
+            {
+                this.puerto = puerto;
+                this.servidor = servidor;
+            }
+
+            public bool Conectar()
+            {
+                try
+                {
+                    this.conCliente = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(this.servidor), this.puerto);
+                    this.conCliente.Connect(ipEndPoint);
+                    Stream stream = new NetworkStream(this.conCliente);
+                    this.writer = new StreamWriter(stream);
+                    this.reader = new StreamReader(stream);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            public void Desconectar()
+            {
+                this.conCliente.Close();
+            }
+
+            public void Escribir(string mensaje)
+            {
+                this.writer.WriteLine(mensaje);
+                this.writer.Flush();
+            }
+            public string Leer()
+            {
+                return this.reader.ReadLine().Trim();
+            }
+
+        }
+    }
+}
+}
